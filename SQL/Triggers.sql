36472bbe58289c04eb876424b35f4c93d7eef36f
Evan Li <74785632+evanli2002@users.noreply.github.com>
2022-11-28 11:55:18 -0500

    Update Triggers.sql

--- SQL/Triggers.sql
+++ SQL/Triggers.sql
@@ -1,1 +1,6 @@
-
+-- creates a trigger which executes the restock() function
+-- each time any book in the Book relation has its stock updated
+CREATE OR REPLACE TRIGGER lowStock
+AFTER UPDATE OF stock ON Book 
+FOR EACH ROW
+EXECUTE PROCEDURE restock();
